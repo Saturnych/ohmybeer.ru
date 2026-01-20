07d31f6b862d9f14f730bff221bb857146f8f20e
Saturnych <169702+Saturnych@users.noreply.github.com>
2026-01-20 16:35:10 +0300

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  outDir: './build', // ./distr
+  //outDir: './distr',
   publicDir: './static', // .public
 });
