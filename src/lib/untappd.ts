646813b217a1f204fcfc891021b5dc046bd4ebdd
Saturnych <169702+Saturnych@users.noreply.github.com>
2026-01-22 07:10:27 +0300

    Update untappd.ts

--- src/lib/untappd.ts
+++ src/lib/untappd.ts
@@ -7,6 +7,8 @@ const {
 } =
 	import.meta.env;
 console.log(` PRIVATE_UNTAPPD_API_URL.length:`, PRIVATE_UNTAPPD_API_URL?.length);
+console.log(` PRIVATE_UNTAPPD_CLIENT_ID.length:`, PRIVATE_UNTAPPD_CLIENT_ID?.length);
+console.log(` PRIVATE_UNTAPPD_CLIENT_SECRET.length:`, PRIVATE_UNTAPPD_CLIENT_SECRET?.length);
 
 // https://api.untappd.com/v4/method_name?client_id=CLIENTID&client_secret=CLIENTSECRET
 // https://api.untappd.com/v4/search/beer?q=Pliny
