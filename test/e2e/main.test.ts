55b8179256b13aa64329843391e24f8ec61b9eb0
Denis <saturnych@gmail.com>
2026-02-04 02:31:23 +0300

    feat: added tests

--- test/e2e/main.test.ts
+++ test/e2e/main.test.ts
@@ -1,13 +1,12 @@
 import { expect, test, type Page } from '@playwright/test';
 import { config } from 'dotenv';
 import { isValidUrl, sleep } from '../../src/lib/utils';
-import pkg from '../../package.json' with { type: 'json' };
 
 const DEBUG = process.env.NODE_ENV !== 'production';
 const TIMEOUT = 3000;
 
 if (DEBUG) config({ quiet: true });
-const { PRIVATE_PRICE_URL } = process.env;
+const { PRIVATE_PRICE_TITLE, PRIVATE_PRICE_URL } = process.env;
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 test.describe.configure({ mode: 'serial' });
@@ -15,7 +14,7 @@ test.describe.configure({ mode: 'serial' });
 let page: Page;
 
 test.beforeAll(async ({ browser }) => {
-	console.log('PRIVATE_PRICE_URL:', PRIVATE_PRICE_URL);
+	console.log('PRIVATE_PRICE_TITLE:', PRIVATE_PRICE_TITLE);
 	page = await browser.newPage();
 });
 
@@ -26,14 +25,19 @@ test.afterAll(async () => {
 
 test('price page check', async () => {
 	test.slow();
-	if (!PRIVATE_PRICE_URL) return;
+	if (!isValidUrl(PRIVATE_PRICE_URL)) return;
 	await sleep(TIMEOUT);
 	await page.goto(PRIVATE_PRICE_URL, { waitUntil: 'domcontentloaded' });
 	const title = await page.title();
 	console.log('price page title:', title);
-	//await expect(title).toBe(pkg.title);
-	const content = await page.content();
-	console.log('price page content:', content);
-	const priceTable = await page.getByRole('div', { id: 'table-row' });
-	await expect(priceTable).toBeVisible();
+	await expect(title).toBe(PRIVATE_PRICE_TITLE);
+	const h1 = page.locator('h1');
+	await expect(h1).toBeVisible();
+	await expect(h1).toHaveText(PRIVATE_PRICE_TITLE);
+	// updateDate = await page.getByRole('p', { id: 'update-date' });
+	//const updated = await updateDate.textContent();
+	//console.log('updated:', updated);
+	//await expect(updateDate).toHaveText('Обновлено');
+	//const content = await page.content();
+	//console.log('price page content:', content);
 });
