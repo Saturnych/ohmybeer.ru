642187813f136e4252fb9447eced1d5d3ba61a44
Denis <saturnych@gmail.com>
2026-02-05 13:57:08 +0300

    taps feed updated

--- src/pages/taps/feed.xml.ts
+++ src/pages/taps/feed.xml.ts
@@ -4,7 +4,11 @@ import { encodeHTMLEntities } from '$/lib/utils';
 import pkg from '$/lib/pkg';
 
 const getYMLFeed = async (site: string = pkg.homepage): Promise<string> => {
-	const offers: Record<string, any>[] = [];
+	const categories = [
+		{ id: 101, name: 'Пиво', find: [] },
+		{ id: 102, name: 'Сидр', find: ['сидр', 'cider'] },
+	];
+
 	const beersCollection = await getCollection('beers');
 	const beers: Record<string, any>[] = beersCollection.map((item) => item.data);
 	console.log(beers?.length);
@@ -12,46 +16,77 @@ const getYMLFeed = async (site: string = pkg.homepage): Promise<string> => {
 	const taps: Record<string, any>[] = tapsCollection.map((item) => item.data);
 	console.log(taps?.length);
 
-	const found = taps.find((tap) => tap.beer_slug === 'midnight-project-zhigulikkeller');
-	console.log(found);
+	const offers: Record<string, any>[] = [
+		{
+			id: 3318394,
+			available: !!taps.find((tap) => tap.beer_slug === 'white-stone-shvatka'),
+			categoryId: 101,
+			count: 500,
+			measure: 'mlit',
+			price: 1,
+			currencyId: 'RUB',
+			name: 'White Stone - Схватка',
+			vendor: 'White Stone',
+			shortDescription: 'Pale Ale - American, ABV: 5.4, IBU: 50, Чекинов в UNTAPPD: 836',
+			description:
+				'Этот Pale Ale для кого-то резкий и колючий, как третья линия, для кого-то слишком мощный и далёкий, как великаны второй, а для кого-то - уютный и добродушный, как могучие игроки первой. Пиво сварено в честь Регбийного Клуба "Спартак Москва", и если ты держишь эту банку в руках, то ты либо лучший из лучших, либо сказочный везунчик!',
+			url: `${site}beers/white-stone-shvatka/`,
+		},
+		{
+			id: 3069407,
+			available: !!taps.find((tap) => tap.beer_slug === 'midnight-project-zhigulikkeller'),
+			categoryId: 101,
+			count: 500,
+			measure: 'mlit',
+			price: 1,
+			currencyId: 'RUB',
+			name: 'Midnight Project - Zhigulikkeller',
+			vendor: 'Midnight Project',
+			shortDescription:
+				'Lager - IPL (India Pale Lager), ABV: 4.5, IBU: 37, Чекинов в UNTAPPD: 6642',
+			description:
+				'Collaboration w/ Mikkeller and Selfmade brewery. According to GOST but GOES beyond. DDH Citra & Simcoe hops',
+			url: `${site}beers/midnight-project-zhigulikkeller/`,
+		},
+		{
+			id: 1963594,
+			available: !!taps.find((tap) => tap.beer_slug === 'zapovednik-abanamat'),
+			categoryId: categories.find((category) => category.name === 'Сидр')?.id || categories[0].id,
+			count: 500,
+			measure: 'mlit',
+			price: 1,
+			currencyId: 'RUB',
+			name: 'Заповедник - АБАНАМАТ',
+			vendor: 'Заповедник',
+			shortDescription: 'Cider - Dry, ABV: 5, Чекинов в UNTAPPD: 7647',
+			description:
+				'Сухой сидр, яблоки для которого собраны непосредственно на территории и в окрестностях Мемориального музея-заповедника А. С. Пушкина «Михайловское». Часть яблок собрана в саду дома, в котором жил Валерий Карпов, он же Марков из "Заповедника" Сергея Довлатова. Для АБАНАМАТА мы давили сок из Антоновки, Мельбы, Китайки и Лешуги (так в наших местах называют дикую яблоню).',
+			url: `${site}beers/zapovednik-abanamat/`,
+		},
+	];
 
 	return `<yml_catalog>
   <shop>
     <categories>
-      <category id="101">Пиво</category>
-      <category id="102">Сидр</category>
+      ${categories.map((category) => `<category id="${category.id}">${category.name}</category>`).join('\n')}
     </categories>
     <offers>
-      <offer id="3318394">
-        <name>White Stone - Схватка</name>
-        <vendor>White Stone</vendor>
-        <categoryId>101</categoryId>
-        <price>1</price>
-        <currencyId>RUB</currencyId>
-        <description>Этот Pale Ale для кого-то резкий и колючий, как третья линия, для кого-то слишком мощный и далёкий, как великаны второй, а для кого-то - уютный и добродушный, как могучие игроки первой. Пиво сварено в честь Регбийного Клуба "Спартак Москва", и если ты держишь эту банку в руках, то ты либо лучший из лучших, либо сказочный везунчик!</description>
-        <shortDescription>Pale Ale - American, ABV: 5.4, IBU: 50, Чекинов в UNTAPPD: 836</shortDescription>
-        <url>${site}/beers/white-stone-shvatka/</url>
-      </offer>
-      <offer id="3069407"${found ? '' : ' available="unknown"'}>
-        <name>Midnight Project - Zhigulikkeller</name>
-        <vendor>Midnight Project</vendor>
-        <categoryId>101</categoryId>
-        <price>1</price>
-        <currencyId>RUB</currencyId>
-        <description>${encodeHTMLEntities('Collaboration w/ Mikkeller and Selfmade brewery. According to GOST but GOES beyond. DDH Citra & Simcoe hops')}</description>
-        <shortDescription>Lager - IPL (India Pale Lager), ABV: 4.5, IBU: 37, Чекинов в UNTAPPD: 6642</shortDescription>
-        <url>${site}/beers/midnight-project-zhigulikkeller/</url>
-      </offer>
-      <offer id="1963594">
-        <name>Заповедник - АБАНАМАТ</name>
-        <vendor>Заповедник</vendor>
-        <categoryId>102</categoryId>
-        <price>1</price>
-        <currencyId>RUB</currencyId>
-        <description>Сухой сидр, яблоки для которого собраны непосредственно на территории и в окрестностях Мемориального музея-заповедника А. С. Пушкина «Михайловское». Часть яблок собрана в саду дома, в котором жил Валерий Карпов, он же Марков из "Заповедника" Сергея Довлатова. Для АБАНАМАТА мы давили сок из Антоновки, Мельбы, Китайки и Лешуги (так в наших местах называют дикую яблоню).</description>
-        <shortDescription>Cider - Dry, ABV: 5, Чекинов в UNTAPPD: 7647</shortDescription>
-        <url>${site}/beers/zapovednik-abanamat/</url>
-      </offer>
+      ${offers
+				.map(
+					(offer) => `<offer id="${offer.id}"${offer.available ? '' : ' available="unknown"'}>
+				<categoryId>${offer.categoryId}</categoryId>
+				<count>${offer.count}</count>
+        <measure>${offer.measure}</measure>
+        <price>${offer.price}</price>
+        <currencyId>${offer.currencyId}</currencyId>
+        <name>${encodeHTMLEntities(offer.name)}</name>
+        <vendor>${encodeHTMLEntities(offer.vendor)}</vendor>
+        <shortDescription>${encodeHTMLEntities(offer.shortDescription)}</shortDescription>
+        <description>${encodeHTMLEntities(offer.description)}</description>
+        <url>${offer.url}</url>
+      </offer>`,
+				)
+				.join('\n')}
     </offers>
   </shop>
   </yml_catalog>`;
