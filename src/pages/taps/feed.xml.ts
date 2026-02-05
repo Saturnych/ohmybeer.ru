1b50bc583119234b5e437e3cec4354440b53ae68
Denis <saturnych@gmail.com>
2026-02-05 12:53:33 +0300

    Update feed.xml.ts

--- src/pages/taps/feed.xml.ts
+++ src/pages/taps/feed.xml.ts
@@ -26,6 +26,8 @@ const getYMLFeed = async (site: string = pkg.homepage): Promise<string> => {
         <name>White Stone - Схватка</name>
         <vendor>White Stone</vendor>
         <categoryId>101</categoryId>
+        <price>1</price>
+        <currencyId>RUB</currencyId>
         <description>Этот Pale Ale для кого-то резкий и колючий, как третья линия, для кого-то слишком мощный и далёкий, как великаны второй, а для кого-то - уютный и добродушный, как могучие игроки первой. Пиво сварено в честь Регбийного Клуба "Спартак Москва", и если ты держишь эту банку в руках, то ты либо лучший из лучших, либо сказочный везунчик!</description>
         <shortDescription>Pale Ale - American, ABV: 5.4, IBU: 50, Чекинов в UNTAPPD: 836</shortDescription>
         <url>${site}/beers/white-stone-shvatka/</url>
@@ -34,6 +36,8 @@ const getYMLFeed = async (site: string = pkg.homepage): Promise<string> => {
         <name>Midnight Project - Zhigulikkeller</name>
         <vendor>Midnight Project</vendor>
         <categoryId>101</categoryId>
+        <price>1</price>
+        <currencyId>RUB</currencyId>
         <description>${encodeHTMLEntities('Collaboration w/ Mikkeller and Selfmade brewery. According to GOST but GOES beyond. DDH Citra & Simcoe hops')}</description>
         <shortDescription>Lager - IPL (India Pale Lager), ABV: 4.5, IBU: 37, Чекинов в UNTAPPD: 6642</shortDescription>
         <url>${site}/beers/midnight-project-zhigulikkeller/</url>
@@ -42,6 +46,8 @@ const getYMLFeed = async (site: string = pkg.homepage): Promise<string> => {
         <name>Заповедник - АБАНАМАТ</name>
         <vendor>Заповедник</vendor>
         <categoryId>102</categoryId>
+        <price>1</price>
+        <currencyId>RUB</currencyId>
         <description>Сухой сидр, яблоки для которого собраны непосредственно на территории и в окрестностях Мемориального музея-заповедника А. С. Пушкина «Михайловское». Часть яблок собрана в саду дома, в котором жил Валерий Карпов, он же Марков из "Заповедника" Сергея Довлатова. Для АБАНАМАТА мы давили сок из Антоновки, Мельбы, Китайки и Лешуги (так в наших местах называют дикую яблоню).</description>
         <shortDescription>Cider - Dry, ABV: 5, Чекинов в UNTAPPD: 7647</shortDescription>
         <url>${site}/beers/zapovednik-abanamat/</url>
