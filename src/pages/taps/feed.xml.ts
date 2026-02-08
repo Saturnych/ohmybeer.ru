8d596c946b934959ab6bffc5e90257b390c373af
Denis <saturnych@gmail.com>
2026-02-08 16:36:39 +0300

    prices updated

--- src/pages/taps/feed.xml.ts
+++ src/pages/taps/feed.xml.ts
@@ -43,7 +43,7 @@ const getYMLFeed = async (site: string = pkg.homepage): Promise<string> => {
 			measure: 'mlit',
 			price: !!tap?.price ? Math.round(tap.price / 2) : 1,
 			currencyId: 'RUB',
-			name: `${!!tap?.brewery && brewery.brewery_name?.length > 20 ? tap.brewery : brewery.brewery_name} - ${beer.beer_name}`,
+			name: `${!!tap?.brewery && brewery.brewery_name?.length > 20 ? tap.brewery : brewery.brewery_name} - ${!!tap?.beer && beer.beer_name.length > 30 ? tap.beer : beer.beer_name}`,
 			vendor: brewery.brewery_name,
 			shortDescription: `${beer.beer_style}${beer.beer_abv > 0 ? ', ABV: ' + beer.beer_abv : ''}${beer.beer_ibu > 0 ? ', IBU: ' + beer.beer_ibu : ''}, Чекинов в Untappd: ${beer.checkin_count}`,
 			description: beer.beer_description,
