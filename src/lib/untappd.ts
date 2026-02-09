0e9bf5ae3a0db7a0e3cfde3660974b76bea7be86
Denis <saturnych@gmail.com>
2026-02-09 20:42:38 +0300

    added untappdAuthGetToken

--- src/lib/untappd.ts
+++ src/lib/untappd.ts
@@ -1,7 +1,8 @@
 const {
-	PUBLIC_UNTAPPD_API_URL = 'https://api.untappd.com/v4',
-	PUBLIC_UNTAPPD_OAUTH_URL = 'https://untappd.com/oauth/authenticate/',
 	PUBLIC_UNTAPPD_REDIRECT_URL = 'http://localhost:3300',
+	PUBLIC_UNTAPPD_API_URL = 'https://api.untappd.com/v4',
+	PUBLIC_UNTAPPD_OAUTH_URL = 'https://untappd.com/oauth/authenticate',
+	PRIVATE_UNTAPPD_AUTH_URL = 'https://untappd.com/oauth/authorize',
 	PUBLIC_UNTAPPD_CLIENT_ID,
 	PRIVATE_UNTAPPD_CLIENT_ID,
 	PRIVATE_UNTAPPD_CLIENT_SECRET,
@@ -12,17 +13,40 @@ const {
 // https://api.untappd.com/v4/method_name?access_token=ACESSTOKENHERE
 // https://api.untappd.com/v4/search/beer?q=Pliny
 // https://untappd.com/oauth/authenticate/?client_id=CLIENTID&response_type=code&redirect_url=http://localhost:3300
-//
+// https://untappd.com/oauth/authorize/?client_id=CLIENTID&client_secret=CLIENTSECRET&response_type=code&redirect_url=http://localhost:3300&code=CODE
 
 export const untappdOauthUri = (): string => {
-	return `${PUBLIC_UNTAPPD_OAUTH_URL}?client_id=${PUBLIC_UNTAPPD_CLIENT_ID}&response_type=code&redirect_url=${PUBLIC_UNTAPPD_REDIRECT_URL}`;
+	return `${PUBLIC_UNTAPPD_OAUTH_URL}/?client_id=${PUBLIC_UNTAPPD_CLIENT_ID}&response_type=code&redirect_url=${PUBLIC_UNTAPPD_REDIRECT_URL}`;
+};
+
+export const untappdAuthGetToken = async (code: string): Promise<string> => {
+	let access_token: string;
+	try {
+		if ((PRIVATE_UNTAPPD_AUTH_URL || '').length > 0) {
+			const authURI: string = `${PRIVATE_UNTAPPD_AUTH_URL}/?client_id=${PRIVATE_UNTAPPD_CLIENT_ID}&client_secret=${PRIVATE_UNTAPPD_CLIENT_SECRET}&response_type=code&redirect_url=${PUBLIC_UNTAPPD_REDIRECT_URL}&code=${code}`;
+			const response: any = await fetch(authURI, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+			const data = await response.json();
+			access_token =
+				data?.meta?.http_code === 200 && !!data.response?.access_token
+					? data.response?.access_token
+					: '';
+		}
+	} catch (err) {
+		console.error('untappdAuthGetToken error:', err);
+	}
+	return access_token;
 };
 
 export const untappdApiCall = async (
 	method_name: string,
 	qstring?: string,
 	skipToken?: boolean,
-): Record<string, any> => {
+): Promise<Record<string, any>> => {
 	let data: Record<string, any> = {};
 	try {
 		if ((PUBLIC_UNTAPPD_API_URL || '').length > 0) {
@@ -46,6 +70,11 @@ export const untappdApiCall = async (
 };
 
 /*
+untappdAuthGetToken data: {
+  meta: { http_code: 200 },
+  response: { access_token: '111111111111' }
+}
+
 {
   "meta": {
     "code": 200,
